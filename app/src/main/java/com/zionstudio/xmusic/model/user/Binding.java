5e160c4ccf292ece5bef13d49380bf0ef6a7b89d
Zion <ZionLife1025@163.com>
2017-04-26 00:14:50 +0800

    重新上传整个项目至github，完成MusicFragment布局的编写

---
+++ app/src/main/java/com/zionstudio/xmusic/model/user/Binding.java
@@ -0,0 +1,12 @@
+package com.zionstudio.xmusic.model.user;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2017/4/23 0023.
+ */
+
+public class Binding implements Serializable {
+    public String tokenString;
+    public String id;
+}
